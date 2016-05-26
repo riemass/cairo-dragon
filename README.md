1298241a76195872c1e54524f7845a828f83bb31
Samir Halilčević <samir.halilcevic@fet.ba>
2016-05-27 00:36:19 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Dragon curve program
+
+Spreads the dragon's wings over the 2d plane. 
+
+Compile with cairo libs (_make_ should take care of that), run with 
+
+    ./dragon_breath num
+
+for a dragon of depth _num_. Output goes to alduin.svg
---
+++ main.c
@@ -0,0 +1,71 @@
+#include <stdlib.h>
+
+#include <cairo.h>
+#include <cairo-svg.h>
+#include <cairo-pdf.h>
+
+#define SIZE_H 640
+#define SIZE_V 360
+
+void dragon_draw(int, int, int, int);
+void dragon_r(cairo_t * cr, int depth, short *x,short *y);
+void dragon_l(cairo_t * cr, int depth, short *x,short *y);
+
+void dragon_l(cairo_t * cr, int depth, short *x,short *y)
+{
+  if (depth != 0)
+    dragon_l(cr, depth-1, x, y);
+  short t = *x; *x = *y; *y = -t; 
+  cairo_rel_line_to(cr, *x, *y);
+
+  if (depth != 0)
+    dragon_r(cr, depth - 1, x, y);
+}
+
+void dragon_r(cairo_t * cr, int depth, short *x,short *y)
+{
+  if (depth != 0)
+    dragon_l(cr, depth-1, x, y);
+  short t = *x; *x = -*y; *y = t; 
+  cairo_rel_line_to(cr, *x, *y);
+  if (depth != 0)
+    dragon_r(cr, depth -1, x, y);
+}
+
+
+int main(int argc, char **argv)
+{
+	int h_size = SIZE_H;
+	int v_size = SIZE_V;
+	int line_len = 3;
+	int n = 2;
+
+        n = atoi(argv[1]);
+	
+	dragon_draw(h_size, v_size, line_len, n);
+
+	return 0;
+}
+	
+void dragon_draw(int h_size, int v_size, int line_len, int depth)
+{
+	cairo_surface_t *surface;
+	surface = cairo_svg_surface_create("alduin.svg", h_size, v_size);
+	cairo_t * cr;
+	cr = cairo_create(surface);
+	
+	cairo_set_source_rgb(cr, 0, 0, 0);
+	cairo_set_line_width(cr, 0.5);
+	cairo_move_to(cr, h_size/2, v_size/2);
+
+	short x = -line_len, y = 0;
+
+        cairo_rel_line_to(cr, x, y);
+
+        dragon_l(cr, depth, &x, &y);
+	
+	cairo_stroke(cr);	
+	cairo_destroy(cr);	
+	cairo_surface_destroy(surface);
+}
+
